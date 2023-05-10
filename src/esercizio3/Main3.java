8cf8bf43953aaef566b3bb49c22e4beb1c02a5ac
Luigi160397 <luigi.difraia.97@gmail.com>
2023-05-10 16:54:55 +0200

    esercizio3

---
+++ src/esercizio3/Main3.java
@@ -0,0 +1,16 @@
+package esercizio3;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main3 {
+	public static Logger logger = LoggerFactory.getLogger(Main3.class);
+
+	public static void main(String[] args) {
+		RegistroPresenze.aggiungiDipendente("Mario", 3);
+		RegistroPresenze.aggiungiDipendente("Luca", 7);
+		RegistroPresenze.aggiungiDipendente("Giovanni", 1);
+
+	}
+
+}
